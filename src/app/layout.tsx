346280ef96ee5faa9bcdc3e3ef1641c3d68cc936
Navin Narayanan <nan.navinnarayanan@gmail.com>
2025-04-12 01:40:19 -0500

    init ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "@/styles/globals.css";
 import { type Metadata } from "next";
 import { ClerkProvider, SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import { Geist } from "next/font/google";
+import { NuqsAdapter } from "nuqs/adapters/next/app";
 
 export const metadata: Metadata = {
     title: "Create T3 App",
@@ -18,26 +19,28 @@ const geist = Geist({
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
     return (
         <ClerkProvider>
-            <html lang="en">
-                <body className={geist.className}>
-                    <header className="flex justify-between items-center px-6 py-4 shadow-md border-b border-gray-200 bg-white">
-                        <h1 className="text-xl font-semibold">Finverse</h1>
-                        <div>
-                            <SignedOut>
-                                <SignInButton>
-                                    <button className="bg-black text-white font-medium px-4 py-2 rounded-lg hover:bg-gray-900 transition">
-                                        Sign In
-                                    </button>
-                                </SignInButton>
-                            </SignedOut>
-                            <SignedIn>
-                                <UserButton />
-                            </SignedIn>
-                        </div>
-                    </header>
-                    <main className="p-6">{children}</main>
-                </body>
-            </html>
+            <NuqsAdapter>
+                <html lang="en">
+                    <body className={geist.className}>
+                        <header className="flex justify-between items-center px-6 py-4 shadow-md border-b border-gray-200 bg-white">
+                            <h1 className="text-xl font-semibold">Finverse</h1>
+                            <div>
+                                <SignedOut>
+                                    <SignInButton>
+                                        <button className="bg-black text-white font-medium px-4 py-2 rounded-lg hover:bg-gray-900 transition">
+                                            Sign In
+                                        </button>
+                                    </SignInButton>
+                                </SignedOut>
+                                <SignedIn>
+                                    <UserButton />
+                                </SignedIn>
+                            </div>
+                        </header>
+                        <main className="p-6">{children}</main>
+                    </body>
+                </html>
+            </NuqsAdapter>
         </ClerkProvider>
     );
 }
